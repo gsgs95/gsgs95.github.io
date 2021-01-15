a3fcd93556cf53f622132a563b58410f1161d734
gyusu, park <rbtn95@gmail.com>
2021-01-15 09:07:50 +0900

    Painter update 2021-01-15

--- app.js
+++ app.js
@@ -1,24 +1,37 @@
-// Selectors
-const bodySection=document.getElementById("jsBodySection");
-const canvas = document.getElementById("jsCanvas");
-const ctx = canvas.getContext('2d');
-const colors = document.getElementsByClassName("jsColor");
-const tools = document.getElementsByClassName("jsTool");
-const range = document.getElementById("jsRange");
-const bucket = document.getElementById("jsBucket");
-const saveBtn = document.getElementById("jsSave");
-const homeBtn = document.getElementById("jsHome");
-const rangeViewer = document.getElementById("jsRangeViewer");
-
-// Tools
+// constant values
 const COLORS = ["jsColor0", "jsColor1","jsColor2","jsColor3",
-"jsColor4","jsColor5","jsColor6","jsColor7","jsColor8"]
+"jsColor4","jsColor5","jsColor6","jsColor7","jsColor8", "jsColor9"]
 const PEN = "jsPen", 
     ERASER = "jsEraser",
     BUCKET = "jsBucket",
     PIPETTE = "jsPipette";
 const TOOLS = [PEN, ERASER, BUCKET, PIPETTE];
 const CLICKED = "clicked";
+const FREE_COLOR_ID = "jsColor9";
+const TRANSPARENT = "transparent url('./painterImg/bgPattern.png') repeat";
+
+// Selectors
+const bodySection=document.getElementById("jsBodySection");
+const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext('2d');
+const colors = document.getElementsByClassName("jsColor");
+const tools = document.getElementsByClassName("jsTool");
+const saveBtn = document.getElementById("jsSave");
+const homeBtn = document.getElementById("jsHome");
+const bucket = document.getElementById(BUCKET);
+const freeColorBtn = document.getElementById(FREE_COLOR_ID);
+
+// Range Selectors
+const lineWidthRange = document.getElementById("jsLineWidthRange");
+const redRange = document.getElementById("jsRedRange");
+const greenRange = document.getElementById("jsGreenRange");
+const blueRange = document.getElementById("jsBlueRange");
+//const alphaRange = document.getElementById("jsAlphaRange");
+const lineWidthRangeViewer = document.getElementById("jsLineWidthRangeViewer");
+const redRangeViewer = document.getElementById("jsRedRangeViewer");
+const greenRangeViewer = document.getElementById("jsGreenRangeViewer");
+const blueRangeViewer = document.getElementById("jsBlueRangeViewer");
+//const alphaRangeViewer = document.getElementById("jsAlphaRangeViewer");
 
 // default values
 const INITIAL_CANVAS_WIDTH = canvas.offsetWidth;
@@ -36,20 +49,49 @@ ctx.strokeStyle = INITIAL_COLOR;
 ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = INITIAL_LINEWIDTH; 
 
+// line attributes
+ctx.lineCap='round';
+ctx.lineJoin = 'round';
 
 // control flags
+let transparent = true;
 let painting = false;
+
+
 let drawing = true;
 let erasing = false;
 let filling = false;
 let pipetting = false;
 
-//////////////////////////////////////////////////////////////////////////
-// find & update the selection
 
-function updateCurrentSelection(obj){
-    console.log(obj);
+
+//////////////////////////////////////////////////////////////////////////
+/*
+// For making a canvas Background Pattern
+const PATTERN_COLORS = ["#bbbbbb", "#eeeeee"];
+const PATTERN_SIZE=10;
+const wNum = canvas.width/PATTERN_SIZE;
+const hNum = canvas.height/PATTERN_SIZE;
+
+// canvas background imagepattern
+function makeCanvasBackgroundPattern() {
+    //create square pattern
+    for (i = 0; i < hNum; i++) {
+        for (j = 0; j < wNum; j++) {
+            ctx.beginPath();
+            ctx.rect(0 + PATTERN_SIZE * j, 0 + PATTERN_SIZE * i, PATTERN_SIZE, PATTERN_SIZE);
+            if (i % 2 === 1) {
+                ctx.fillStyle = PATTERN_COLORS[`${j % 2}`];
+            } else {
+                ctx.fillStyle = PATTERN_COLORS[`${(j + 1) % 2}`];
+            }
+            //fill the rectangle with the selected color
+            ctx.fill();
+        }
+    }
 }
+*/
+//////////////////////////////////////////////////////////////////////////
 
 // canvasEvent handlers
 function stopPainting() {
@@ -61,30 +103,72 @@ function startPainting() {
 }
 
 function onMouseMove(event) {
-    if (drawing) {
+    if (drawing || erasing) {
         const x = event.offsetX;
         const y = event.offsetY;
         if (!painting) {
             ctx.beginPath();
             ctx.moveTo(x, y);
         } else {
+            if(drawing) { // mouseMove with PenMode
             ctx.lineTo(x, y);
             ctx.stroke();
+            } else { // mouseMove with EraserMode
+                const size = lineWidthRange.value;
+                ctx.clearRect(`${x-size/2}`, `${y-size/2}`, size, size);
+            }            
         }
     }
 }
-
-// canvasFill handler
-function handleCanvasClick() {
-    if (filling) {
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
-    } else if(pipetting) {
-        console.log("Canvas Click Event appears while Pippeting Mode.");
+/////////////////////////////////////////////////////
+/* ================= Using Tools ================= */
+function handleCanvasClick(event) {
+    // 클릭지점의 캔버스 내 좌표얻음
+    const x = event.offsetX;
+    const y = event.offsetY;
+    // 활성화된 모드별로 작동
+    if (filling) { // Bucket Mode
+        ctx.fillRect(0,0,canvas.width,canvas.height);
+    } else if(pipetting) { // Pippet Mode
+        // 클릭지점 픽셀의 색상값을 배열로 받아옴
+        const imageData = ctx.getImageData(x, y, 1,1);
+        const pixelColor=Array.from(imageData.data);
+        const cssColor = "rgba(" + pixelColor[0] + ", " + 
+        pixelColor[1] + ", " + pixelColor[2] + ", " + pixelColor[3] + ")"
+        // 선택된 색상에 의한 RGB Channel Range 변환
+        redRange.value = pixelColor[0];
+        greenRange.value = pixelColor[1];
+        blueRange.value = pixelColor[2];
+        redRangeViewer.innerText=pixelColor[0];
+        greenRangeViewer.innerText=pixelColor[1];
+        blueRangeViewer.innerText=pixelColor[2];
+        // 선택된 색상이 무색인지 검사        
+        if(pixelColor[3] === 0){
+            freeColorBtn.style.background = TRANSPARENT;
+            transparent = true;
+        } else{
+            freeColorBtn.style.background = "";
+            transparent = false;
+        }
+        // 선택된 색상을 자유색 버튼의 색상값에 저장
+        freeColorBtn.style.backgroundColor = cssColor;
+        // 만약 자유색이 선택된 상태라면, fillStyle/strokeStyle 바로 업데이트
+        if (Array.from(freeColorBtn.classList).includes(CLICKED)) {
+            ctx.strokeStyle = cssColor;
+            ctx.fillStyle = cssColor;
+        }      
+    } else if(drawing) { // Pen Mode
+        // mousemove 없이 점만 찍어도 작동해야함.
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    } else if(erasing) { // Eraser Mode
+        const size = lineWidthRange.value;
+        ctx.clearRect(`${x-size/2}`, `${y-size/2}`, size, size);
     }
 }
 
 /////////////////////////////////////////////////////
-/* ================= Click Tools ================= */
+/* ================= Active Tools ================= */
 function initModes() {
     drawing = false;
     erasing = false;
@@ -108,23 +192,20 @@ function handleToolsClick(event){
         initModes();
         switch (id) {
             case PEN:
-                console.log("It is " + PEN);
                 handlePenClick();
                 break;
             case ERASER:
-                console.log("It is " + ERASER);
                 handleEraserClick();
                 break;
             case PIPETTE:
-                console.log("It is " + PIPETTE);
                 handlePipetteClick();
                 break;
             case BUCKET:
-                console.log("It is " + BUCKET);
                 handleBucketClick();
                 break;
             default:
-                console.log("ERROR: unvalid id @handleToolsClick()");                
+                console.log("ERROR: unvalid id @handleToolsClick()");
+                break;           
         }
     }
 }
@@ -167,6 +248,8 @@ function handleColorClick(event) {
         const clickedColor = clickedObj.style.backgroundColor;
         ctx.strokeStyle = clickedColor;
         ctx.fillStyle = clickedColor;
+    } else {
+        console.log("ERROR: unvalid colorBtn id @handleColorClick()");
     }
 }
 
@@ -175,13 +258,44 @@ function handleColorClick(event) {
 
 // rangeChange handler
 function handleRangeChange(event) {
+    // 어떤 range가 변하는지 확인
     const size = event.target.value;
-    rangeViewer.innerText=`${size}`;
-    ctx.lineWidth = size;
+    const sizebox = event.target.nextSibling.nextSibling;
+    console.log(sizebox.id);
+    sizebox.innerText = `${size}`;
+    if(sizebox === lineWidthRangeViewer) { // 선굵기
+        ctx.lineWidth = size;
+    } else { // RGB
+        const currentColor = new String(freeColorBtn.style.backgroundColor);
+        let tmpRGB = [0, 0, 0];
+        let tmpColor
+
+        // 투명관련처리
+        if(currentColor != "transparent"){ 
+            // 현재 자유색값 문자열=> 배열 변환
+            tmpColor = currentColor.slice(4);
+            tmpColor = tmpColor.slice(0, tmpColor.length-1);
+            tmpColor = tmpColor.split(", ");
+            tmpRGB = tmpColor;
+        }
+        switch(sizebox) { // RGB 채널별로 처리
+            case redRangeViewer: tmpRGB[0]=size; break;
+            case greenRangeViewer: tmpRGB[1]=size; break;
+            case blueRangeViewer: tmpRGB[2]=size; break;
+        }
+        // 변경한 배열 => 문자열로 재변환
+        tmpColor = "rgb(" + tmpRGB[0] + ", " + tmpRGB[1] + ", " + tmpRGB[2] + ")";
+        // 변경된 색상 업데이트
+        freeColorBtn.style.background = "";
+        transparent = false;
+        freeColorBtn.style.backgroundColor=tmpColor;
+        ctx.fillStyle = tmpColor;
+        ctx.strokeStyle = tmpColor;
+    }
 }
 
 // mouseRightClick(ContextMenu) handler
-function handleContextMenu(event){
+function handleContextMenu(event){ // 마우스 우클릭 블로킹
     event.preventDefault();
 }
 
@@ -204,6 +318,11 @@ function handleHomeClick() {
 //////////////////////////////////////////////////////////////////////////
 // Binding eventListners
 function init() {
+    // initialize background pattern
+    freeColorBtn.style.background = TRANSPARENT;
+    canvas.style.background = TRANSPARENT;
+    // saving initial property of canvas context
+    ctx.save();
     // initialize tool/color selection
     bodySection.style.cursor = "url(./cursors/pen.cur), auto";
     document.getElementById(TOOLS[INITIAL_TOOL_NUM]).classList.add(CLICKED);
@@ -230,10 +349,26 @@ function init() {
     );
     
     // RangeChange event
-    if(range) {
-        range.addEventListener("input", handleRangeChange);
+    if(lineWidthRange) {
+        lineWidthRange.addEventListener("input", handleRangeChange);
     }
 
+    if(redRange) {
+        redRange.addEventListener("input", handleRangeChange);
+    }
+
+    if(greenRange) {
+        greenRange.addEventListener("input", handleRangeChange);
+    }
+
+    if(blueRange) {
+        blueRange.addEventListener("input", handleRangeChange);
+    }
+/*
+    if(alphaRange) {
+        alphaRange.addEventListener("input", handleRangeChange);
+    }
+*/
     // Saving event
     if(saveBtn) {
         saveBtn.addEventListener("click", handleSaveClick);
@@ -254,13 +389,16 @@ init();
             ********* TODO LIST *********
         ✔    1. Range Value Display
         ✔    2. FILL 디버그 (Fill 상태에서 드래그시 mousemove 이벤트 핸들러 작동)
-             3. 투명 & white 구분
+        ✔    3. 투명 & white 구분
              4. 새 캔버스
              5. 캔버스 크기조절 옵션
-             6. 지우개
-             7. 자유색
-             8. 스포이드
+        ✔    6. 지우개
+        ✔    7. 자유색
+        ✔    8. 스포이드
              9. undo redo
         ✔    10. 도구별 커서
+             11. 도구별 단축키 설정
+
+        ⚠  자유색 알파값 조절실패
 */
 //////////////////////////////////////////////////////////////////////////
